60d97ffc9ed5d74ffa9fb9966344cf52a6f3780e
Sungwoo Park <codest99@gmail.com>
2020-07-11 21:17:10 +0900

    test minilibx functions

---
+++ includes/minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunpark <sunpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/11 21:12:59 by sunpark           #+#    #+#             */
+/*   Updated: 2020/07/11 21:13:31 by sunpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+typedef struct	s_data {
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_data;
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunpark <sunpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/11 21:12:06 by sunpark           #+#    #+#             */
+/*   Updated: 2020/07/11 21:16:45 by sunpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "mlx.h"
+#include "minirt.h"
 
-int     main(void)
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    void    *mlx;
-    void    *mlx_win;
+	char	*dst;
 
-    mlx = mlx_init();
-	ft_printf("Hello %d MLX!\n", 10);
-    mlx_win = mlx_new_window(mlx, 500, 500, "Hello MLX!");
-    mlx_loop(mlx);
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int				main(void)
+{
+	void	*mlx;
+	t_data	*img;
+	void	*mlx_win;
+	int		i;
+	int		j;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "miniRT");
+	img = (t_data *)malloc(sizeof(t_data));
+	img->img = mlx_new_image(mlx, 1000, 1000);
+	img->addr = mlx_get_data_addr(img->img, &(img->bits_per_pixel), \
+					&(img->line_length), &(img->endian));
+	i = 99;
+	while (++i < 200)
+	{
+		j = 99;
+		while (++j < 200)
+			my_mlx_pixel_put(img, i, j, 0x00FF0000);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img->img, 0, 0);
+	mlx_loop(mlx);
+	free(img);
 }
